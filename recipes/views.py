f11dc50551c37df5e9941f8caceac810da4dc27e
Rodrigo Caliman <rcaliman@gmail.com>
2022-06-11 22:57:53 -0300

    limpeza para continuar o curso

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name': 'Rodrigo Caliman',
-    })
+    return render(request, 'recipes/home.html')
 
-
-def contato(request):
-    return render(request, 'temp/temp.html')
-
-
-def sobre(request):
-    return HttpResponse("SOBRE")
