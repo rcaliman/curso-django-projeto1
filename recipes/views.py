e507e2cc90082b3ee5431bdb0fa8c945bfcfeb79
Rodrigo Caliman <rcaliman@gmail.com>
2022-06-12 20:10:28 -0300

    alteração nos templates"

--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', {'name':'Rodrigo Caliman'})
+    return render(request, 'recipes/pages/home.html', {'name':'Rodrigo Caliman'})
 
